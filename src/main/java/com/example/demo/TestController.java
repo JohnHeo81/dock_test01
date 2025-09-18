2f8ea479e6481ceb555afa4ef06625e4ccba29e6
John Heo <jungwoocity0327@gmail.com>
2025-09-18 12:42:24 +0900

    test

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public ResponseEntity test() {
-        return ResponseEntity.ok("테스트17");
+        return ResponseEntity.ok("테스트18");
     }
 
     @GetMapping("/health")
